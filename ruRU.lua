5b068047be8e8da8f5348c0d291aea50737af271
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2020-05-25 08:56:10 +0300

    Add files via upload

---
+++ ruRU.lua
@@ -0,0 +1,23 @@
+﻿if GetLocale() ~= "ruRU" then return end
+
+EEL_LocaleText["fav"] = "избранное"
+EEL_LocaleText["lclick"] = "нажмите ЛКМ, чтобы |cFF00FF00открыть"
+EEL_LocaleText["rclick"] = "нажмите ПКМ, чтобы |cFF00FF00переместить"
+EEL_LocaleText["allstats"] = "Общая статистика"
+EEL_LocaleText["pvpmight"] = "Можно в PVP"
+EEL_LocaleText["rangeweapon"] = "Оружие: Дальний бой"
+EEL_LocaleText["runspeed"] = "скорость бега"
+EEL_LocaleText["profession"] = "профессия"
+EEL_LocaleText["leech"] = "Cамоисцеление"
+
+EEL_Fav = {
+	[0] = "Показать избранное",
+	[1] = " ",
+	[2] = "CTRL + Нажмите на наложение чар, чтобы сохранить его в список избранного"
+}
+
+EEL_Help = {
+	[0] = "EpicEnchantList",
+	[1] = " ",
+	[2] = "все вкл. BfA обновляется"
+}
